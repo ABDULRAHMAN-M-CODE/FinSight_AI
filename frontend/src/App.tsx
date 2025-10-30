0e5a2d5cb7af2fe79ad7cb44c843528e4fd85129
ABDULRAHMAN-M-CODE <abdklaib233@gmail.com>
2025-10-30 19:36:03 +0300

    Updated App.tsx
    I'm still creating the Welcome Page ,I made the React My self , I got the CSS from Internet ! tell me what you think !

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+  
+  const [page,setPage]=useState("Welcome_Page")
+    if (page=="Welcome_Page"){
+    return(
+      <div id="WelcomePage">
+        <div id="Frame_133539">
+            <div id="Text">
+              <p id='Welcoming'>Welcome to FinSight AI</p>
+              <p id="describe_services">Unlock personalized financial insights, track your goals, and get smart, AI-powered recommendations tailored just for you—all in one easy-to-use app</p>
+            </div>
+            <div id="Split_Line"></div>
+            <button id='Continue'>Start Your Journey</button>
+        </div>
+      </div>
+    );
+  }
+  else{
+    return(
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Continue......</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
+  
 }
 
 export default App
