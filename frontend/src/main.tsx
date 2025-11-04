0f813353340d81c4a98b4761937c42e6a7dfe11b
ABDULRAHMAN-M-CODE <abdklaib233@gmail.com>
2025-11-04 18:00:02 +0300

    made comments
    just made some comments for my self  about Redux Store

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,11 @@
+import { createRoot } from 'react-dom/client'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import {store} from './store.ts'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
