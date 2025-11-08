423ea05cd6e00fd773427d77f2ad549fa58d92af
ABDULRAHMAN-M-CODE <abdklaib233@gmail.com>
2025-11-08 14:03:08 +0300

    Still Working on Login Page
    Files Updated :
    App.tsx
    App.tsx
    
    Files Created :
    IntroStepper.tsx
    
    I used Ready-made Component from Internet called Stepper UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import { useDispatch } from 'react-redux';
 import { setPage } from './mainSlice';
 import './App.css'
 import { Routes,Route,useNavigate } from 'react-router-dom';
+import IntroStepper from './IntroStepper';
+
 
 function WelcomePage(){
   const dispatch=useDispatch();    
@@ -18,7 +20,7 @@ function WelcomePage(){
             </div>
             <div id="Split_Line"></div>
             <button id='Continue' onClick={()=>{
-              dispatch(setPage("continue...."))
+              dispatch(setPage("Login"))
               navigate("/Login")
             }}>Start Your Journey</button>
         </div>
@@ -27,6 +29,8 @@ function WelcomePage(){
 
   }
   function Login(){
+   const dispatch=useDispatch();    
+  const navigate=useNavigate();
     return(
       <>
         <div id='Login_Frame'>
@@ -41,7 +45,10 @@ function WelcomePage(){
                 <div id='Password_Div'>
                   <div id='Password_Header'>
                     <p id='Password_Label'>Password</p>
-                    <Link to="/Forgot_Password" id="Right_text">Forgot Password?</Link>                    
+                    <Link to="/Forgot_Password" id="Right_text" onClick={()=>
+                      dispatch(setPage("Forgot_Password"))
+                      
+                    }>Forgot Password?</Link>                    
                   </div>
                   <div  id='Password_Input'>
                     <input  id="text" />
@@ -56,7 +63,10 @@ function WelcomePage(){
                   <input  type='checkbox' id="Tick"/>
                   <p id="Keep_me_signed_in">Keep me signed in</p>
                 </div>
-                <button id='Login_Button'>Login</button>
+                <button id='Login_Button' onClick={()=>
+                  
+                  navigate("/IntroStepper")
+                }>Login</button>
               </div>
             </div>
             <div id='Divider_2'>
@@ -66,7 +76,10 @@ function WelcomePage(){
 
               </div>
             </div>
-            <button id="Button_Secondry">
+            <button id="Button_Secondry" onClick={()=>{
+              dispatch(setPage("SignUp_With_Google"))
+              navigate("/SignUp_With_Google")
+            }}>
               <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google G" width="30" height="30"  id="gogle_icon"/>
 
               <p id="Continue_With_Google">Continue With Google</p>
@@ -78,6 +91,34 @@ function WelcomePage(){
       </>
     );
   }
+  function ForgotPassword(){
+    return(
+      <>
+      <button>lets make u new password</button>
+      </>
+    );
+  }
+  function Form(){
+    return(
+      <>
+        <button>lets ask u couple of questions</button>
+      </>
+    );
+  }
+  function SignUp(){
+    return(
+      <>
+        <button>lets make u account</button>
+      </>
+    );
+  }
+  function SignUpWithGoogle(){
+    return(
+      <>
+        <button>Page no completed</button>
+      </>
+    );
+  }
 function App() {
   
     
@@ -86,6 +127,12 @@ function App() {
           <Routes>
             <Route path="/" element={<WelcomePage />}/>
             <Route path='/Login' element={<Login />} />
+            <Route path="Forgot_Password" element={ <ForgotPassword /> } />
+            <Route path="/IntroStepper"  element={<IntroStepper />}/>
+            <Route path="/Form"  element={<Form />}/>
+            <Route path="/Signup"  element={<SignUp/>}/>
+            <Route path="/SignUp_With_Google"  element={<SignUpWithGoogle/>}/>
+            
           </Routes>
         
         );
